e4210486657996d1d0bf55af2ec34e928bfeed09
Muharrem <123410332+MuharremK0@users.noreply.github.com>
2023-11-15 17:34:54 +0300

    Update Lab03part2.asm

--- emu8086_labs/lab_03/Lab03part2.asm
+++ emu8086_labs/lab_03/Lab03part2.asm
@@ -1,7 +1,4 @@
-
-; You may customize this and other start-up templates; 
-; The location of this template is c:\emu8086\inc\0_com_template.txt
-
+;Perfect Square Check
 org 100h
 
 mov ax,n
@@ -13,10 +10,7 @@ mov ax,1
 mov bx,1
 mov cx,n
 sub cx,1
-;mov cx,si
-;dec cx
 
-   
 loop1:
    mov ax,bx
    mul ax
